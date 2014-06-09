49896b0628d830394de3021fc0e85a0108506112
Jaime Sanchez <josejaime.sanchez@gmail.com>
2014-06-09 19:49:01 +1000

    adding jshint and watch tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,42 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
     qunit: {
       files: ['src/test/*.html']
+    },
+    jshint: {
+      files: [
+        'Gruntfile.js', 
+        'src/main/js/*.js', 
+        'src/main/js/**/*.js', 
+        'src/test/*.js', 
+        'src/test/**/*.js', 
+        '!src/main/js/lib/require/require.js', 
+        '!src/main/js/lib/require/text.js', 
+        '!src/main/js/lib/backbone.js', 
+        '!src/main/js/lib/jquery.js', 
+        '!src/main/js/lib/underscore.js', 
+        '!src/test/qunit.js'
+      ],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'qunit']
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('test', ['qunit']);
-
-  grunt.registerTask('default', ['qunit']);
-
+  grunt.registerTask('test', ['jshint', 'qunit']);
 };
